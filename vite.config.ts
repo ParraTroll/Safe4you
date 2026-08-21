6a6c20eb623944f1a5ffe32e95c49efab4206d5d
ParraTroll <121721550+ParraTroll@users.noreply.github.com>
2026-08-21 19:47:13 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,12 @@ import path from "path";
 
 // https://vitejs.dev
 export default defineConfig({
-  base: "/online-shield-spark/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // This bypasses strict errors during local build steps if needed
   build: {
     chunkSizeWarningLimit: 1000,
   }
